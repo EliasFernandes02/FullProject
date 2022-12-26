110a9fecb24e81c27056615ded20ffdc3e464969
Elias Fernandes <eliasneto0014@gmail.com>
2022-12-26 12:45:00 -0300

    Adicionando layouts

--- react/src/router.jsx
+++ react/src/router.jsx
@@ -1,21 +1,47 @@
-import {createBrowserRouter} from "react-router-dom";
+import {createBrowserRouter, Navigate} from "react-router-dom";
 import Login from "./views/Login";
 import SingUp from "./views/SingUp";
 import Users from "./views/Users";
 import Notfound from "./views/Notfound";
+import DefaultLayout from "./Components/DefaultLayout";
+import GuestLayout from "./Components/GuestLayout";
+import Dashboard from "./views/Dashboard";
 
  const router = createBrowserRouter([
      {
-         path:'/login',
-         element:<Login/>
+         path:'/',
+         element:<DefaultLayout/>,
+         children:[
+             {
+                 path:'/',
+                 element:<Navigate to="/users"/>
+             },
+             {
+                 path:'/users',
+                 element:<Users/>
+             },
+             {
+                 path:'/dashboard',
+                 element:<Dashboard/>
+             },
+
+         ]
      },
      {
-         path:'/singup',
-         element:<SingUp/>
-     },
-     {
-         path:'/users',
-         element:<Users/>
+         path:'/',
+         element:<GuestLayout/>,
+         children:
+         [
+             {
+                 path:'/login',
+                 element:<Login/>
+             },
+             {
+                 path:'/singup',
+                 element:<SingUp/>
+             },
+
+         ]
      },
      {
          path:'*',
