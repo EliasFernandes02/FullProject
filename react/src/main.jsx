f1afe307ad3b05b9e7d3e1f66584aac099035350
Elias Fernandes <eliasneto0014@gmail.com>
2022-12-28 11:06:30 -0300

    creating safety routes

--- react/src/main.jsx
+++ react/src/main.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import {ContextProvider} from "./Context/ContextProvider";
+import router from "./router.jsx";
+import {RouterProvider} from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+      <ContextProvider>
+          <RouterProvider router={router}/>
+      </ContextProvider>
+
   </React.StrictMode>,
 )
